4c45ef12a9ca68d8908ba0aefe5cc091a75646a8
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2024-03-18 23:28:51 +0100

    Using Gemma on GTX 1060

--- src/ollama/bhoomi/main.py
+++ src/ollama/bhoomi/main.py
@@ -2,14 +2,19 @@ print("Welcome to Bhoomi")
 
 from langchain_community.llms import Ollama
 
-llm = Ollama(model="mistral")
+llm = Ollama(model="gemma:2b")
 
 # test 1
 
 # llm.invoke("Tell me a joke")
 
 # test 2
-query = "Tell me a joke"
+query = "What are the top 3 important facilities required for a Robot to survive on Mars?"
 
-for chunks in llm.stream(query):
+query2 = "What top 5 Sensors are necessary for Robots on Mars?"
+
+query3 = "Write a python program to connect the 5 sensors"
+
+
+for chunks in llm.stream(query2):
     print(chunks)
