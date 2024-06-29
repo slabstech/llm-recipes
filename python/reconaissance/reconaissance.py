076aa8a3116e32f6361cd1eaa09ec1c7728db87b
sachin <>
2024-06-29 22:52:12 +0200

    draft = generate report

--- python/reconaissance/reconaissance.py
+++ python/reconaissance/reconaissance.py
@@ -7,6 +7,18 @@ import json
 import datetime
 import os
 import requests
+from PIL import Image
+
+
+def get_image_exif():
+    # Open the image file
+    with Image.open('drone_control/picture.png') as img:
+        # Get the image metadata
+        metadata = img.info
+
+    # Print the metadata
+    print(metadata)
+
 
 def get_drone_video():
     print("getting drone video")
@@ -43,31 +55,45 @@ def process_metadata():
 
 def generate_insights(image_metadata):
     # Mistral API endpoint
-    image_metadata = '{"time": "2024-06-29 19:14:36", "drone_picture": "drone_control/picture.png", "image_metadata": "\nThe image shows a group of people standing in an airport terminal. There are at least six individuals visible, with some closer to the foreground and others further back. They appear to be waiting or preparing for their flights, as they stand near chairs and dining tables that can be seen throughout the scene. The chairs and tables suggest that this is likely a waiting area within the terminal."}'
-    prompt = f"Please analyze the following image metadata and provide additional insights: {json.dumps(image_metadata)}"
+#    image_metadata = '{"time": "2024-06-29 19:14:36", "drone_picture": "drone_control/picture.png", "image_metadata": "\nThe image shows a group of people standing in an airport terminal. There are at least six individuals visible, with some closer to the foreground and others further back. They appear to be waiting or preparing for their flights, as they stand near chairs and dining tables that can be seen throughout the scene. The chairs and tables suggest that this is likely a waiting area within the terminal."}'
+    # Load data from JSON file
+    with open('data.json', 'r') as f:
+        data = json.load(f)
 
-    #print(text)
-    ollama_endpoint_env = os.environ.get('OLLAMA_ENDPOINT')
+    prompt = f"Please analyze the following json data : {data}, return short info if there is any difference across time"
 
-    model = "mistral:7b"
-    #prompt = text 
-    if ollama_endpoint_env is None:
-        ollama_endpoint_env = 'http://localhost:11434'
-    ollama_endpoint = ollama_endpoint_env +  "/api/generate"  # replace with localhost
+    ollama_url = "http://localhost:11434"
+    model_name = "mistral"
 
-    response = requests.post(ollama_endpoint,
-                      json={
-                          'model': model,
-                          'prompt': prompt,
-                          'stream':False,
-                          'raw': False
-                      }, stream=False
-                      )
+    command = "/api/generate"
+    url = ollama_url + command
+    model = model_name + ":latest"
+    payload = {"model": model, "prompt":prompt}
     
-    response.raise_for_status()
-    result = response.json()
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
 
-    print(result)
+    response_data = ""
+    if response.status_code == 200:
+        for chunk in response.iter_lines():
+            if chunk:
+                data = chunk.decode('utf-8')
+                data_list = json.loads(data)
+                content = data_list['response']
+                response_data += content
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+
+    print(response_data)
+    # Open the file in append mode ('a')
+    with open('response_data.txt', 'a') as file:
+    # Write the response data to the file
+        file.write(str(response_data))
+    return response_data
+    
 
 def main():
     #drone_video = get_drone_video()
@@ -91,6 +117,7 @@ def main():
     """
 
     process_metadata()
+    # get_image_exif()
 
 if __name__ == "__main__":
     main()
