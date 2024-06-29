3c3d8ec1813e1679bcacbfeef0329f28b8a687c3
sachin <>
2024-06-29 21:59:49 +0200

    draft = insgihts

--- python/assistant/vision_query.py
+++ python/assistant/vision_query.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import json 
 def explain_image(image_path, model, prompt, ollama_url):
         
     with open(image_path, "rb") as image_file:
@@ -22,14 +23,19 @@ def explain_image(image_path, model, prompt, ollama_url):
     }
     
     response = requests.post(url, json=payload, headers=headers)
-    
+
+    response_data = ""
     if response.status_code == 200:
         for chunk in response.iter_lines():
             if chunk:
                 data = chunk.decode('utf-8')
-                print(data)
+                data_list = json.loads(data)
+                content = data_list['message']['content']
+                response_data += content
     else:
         print(f"Error: {response.status_code} - {response.text}")
+    return response_data
+    
 
 def main():
 
--- python/reconaissance/reconaissance.py
+++ python/reconaissance/reconaissance.py
@@ -4,8 +4,9 @@ from drone_control.vision_query import explain_image
 from battlefield.build_map import show_map
 from drone_control.model_setup import load_model
 import json
-
 import datetime
+import os
+import requests
 
 def get_drone_video():
     print("getting drone video")
@@ -19,25 +20,8 @@ def get_drone_picture():
     image_capture(file_name_for_image)
     return file_name_for_image
     
-def main():
-    #drone_video = get_drone_video()
-    #drone_picture = get_drone_picture()    
-
-    ollama_url = "http://localhost:11434"
-    model_name = "moondream"
-    #load_model( ollama_url, model_name )
-
-    drone_picture = "drone_control/picture.png"
-
-    model = "moondream"
-    prompt = "What is in this image?"
-            
-    url = "http://localhost:11434"
-        
-    image_metadata = explain_image(drone_picture, model, prompt, url)    
-    #print(image_metadata)
-    
-    # Get the current time
+def store_image_metadata(image_metadata, drone_picture):
+        # Get the current time
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # Create a dictionary to store the data
@@ -53,7 +37,60 @@ def main():
         f.write('\n')
         #show_map()
 
+def process_metadata():
+    generate_insights("ss")
 
+
+def generate_insights(image_metadata):
+    # Mistral API endpoint
+    image_metadata = '{"time": "2024-06-29 19:14:36", "drone_picture": "drone_control/picture.png", "image_metadata": "\nThe image shows a group of people standing in an airport terminal. There are at least six individuals visible, with some closer to the foreground and others further back. They appear to be waiting or preparing for their flights, as they stand near chairs and dining tables that can be seen throughout the scene. The chairs and tables suggest that this is likely a waiting area within the terminal."}'
+    prompt = f"Please analyze the following image metadata and provide additional insights: {json.dumps(image_metadata)}"
+
+    #print(text)
+    ollama_endpoint_env = os.environ.get('OLLAMA_ENDPOINT')
+
+    model = "mistral:7b"
+    #prompt = text 
+    if ollama_endpoint_env is None:
+        ollama_endpoint_env = 'http://localhost:11434'
+    ollama_endpoint = ollama_endpoint_env +  "/api/generate"  # replace with localhost
+
+    response = requests.post(ollama_endpoint,
+                      json={
+                          'model': model,
+                          'prompt': prompt,
+                          'stream':False,
+                          'raw': False
+                      }, stream=False
+                      )
+    
+    response.raise_for_status()
+    result = response.json()
+
+    print(result)
+
+def main():
+    #drone_video = get_drone_video()
+    #drone_picture = get_drone_picture()    
+    """
+    ollama_url = "http://localhost:11434"
+    model_name = "mistral"
+    load_model( ollama_url, model_name )
+
+    drone_picture = "drone_control/picture.png"
+    """
+    """
+    model = "moondream"
+    prompt = "What is in this image?"
+            
+    url = "http://localhost:11434"
+        
+    image_metadata = explain_image(drone_picture, model, prompt, url)    
+    
+    store_image_metadata(image_metadata, drone_picture)
+    """
+
+    process_metadata()
 
 if __name__ == "__main__":
     main()
