1fa7e06d9e5ac925076f3c0b5cbae595a82e6da5
sachin <>
2024-08-10 03:20:45 +0200

    add to huggingface

---
+++ tutorials/hf-space/README.md
@@ -0,0 +1,18 @@
+---
+title: Codestral Api
+emoji: 🐠
+colorFrom: green
+colorTo: purple
+sdk: gradio
+sdk_version: 4.41.0
+app_file: app.py
+pinned: false
+license: apache-2.0
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+
+---
+
+
+https://huggingface.co/spaces/gaganyatri/codestral-api
---
+++ tutorials/hf-space/app.py
@@ -0,0 +1,21 @@
+import gradio as gr
+from transformers import AutoTokenizer, Mamba2ForCausalLM
+
+model_id = 'gaganyatri/codestral-7B'
+
+# Load the Mamba model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_id, from_slow=True, legacy=False)
+
+model = Mamba2ForCausalLM.from_pretrained(model_id)
+
+# Define the chatbot function
+def chat(input_text):
+    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+    output = model.generate(input_ids, max_length=2500)
+    return tokenizer.batch_decode(output)
+
+# Create the Gradio interface
+demo = gr.Interface(fn=chat, inputs="text", outputs="text")
+
+# Launch the app
+demo.launch(share=True)
