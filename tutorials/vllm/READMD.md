91468a46ca280dff4b2a5b4241657c835e1e3c3c
sachin <>
2024-08-16 02:05:52 +0200

    trial - vllm compressor

---
+++ tutorials/vllm/READMD.md
@@ -0,0 +1,13 @@
+VLLM
+
+llmcompressppor for codestral-mamba
+
+- pip install llmcompressor
+
+-  huggingface-cli download TinyLlama/TinyLlama-1.1B-Chat-v1.0
+
+- python tiny_llama.py
+
+
+- Reference
+    - https://github.com/vllm-project/llm-compressor/
---
+++ tutorials/vllm/tiny_llama.py
@@ -0,0 +1,23 @@
+from llmcompressor.modifiers.quantization import GPTQModifier
+from llmcompressor.modifiers.smoothquant import SmoothQuantModifier
+from llmcompressor.transformers import oneshot
+
+# Select quantization algorithm. In this case, we:
+#   * apply SmoothQuant to make the activations easier to quantize
+#   * quantize the weights to int8 with GPTQ (static per channel)
+#   * quantize the activations to int8 (dynamic per token)
+recipe = [
+    SmoothQuantModifier(smoothing_strength=0.8),
+    GPTQModifier(scheme="W8A8", targets="Linear", ignore=["lm_head"]),
+]
+
+# Apply quantization using the built in open_platypus dataset.
+#   * See examples for demos showing how to pass a custom calibration set
+oneshot(
+    model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    dataset="open_platypus",
+    recipe=recipe,
+    output_dir="TinyLlama-1.1B-Chat-v1.0-INT8",
+    max_seq_length=2048,
+    num_calibration_samples=512,
+)
