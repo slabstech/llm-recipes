cdb6315be750af06bd0ce473dbff80ffbc8a694a
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-02-05 18:17:51 +0100

    init - script parser

---
+++ python/notebooklm/audiobook.py
@@ -0,0 +1,11 @@
+from utils.pdf_parser import parser_data
+
+def main():
+    print("Audiobook Creation from Script")
+    local_filename = 'Skript-GoAudio-Eng.pdf'
+    parser_data(local_filename)
+
+
+
+if __name__ == "__main__":
+    main()
