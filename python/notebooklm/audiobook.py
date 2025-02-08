0074155261867977f1db1ef8d878a809ee2a2202
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-02-08 10:09:26 +0100

    add - missedlanguage-prompts

--- python/notebooklm/audiobook.py
+++ python/notebooklm/audiobook.py
@@ -4,26 +4,14 @@ import requests
 import ast
 import json
 import os
+from mistralai import Mistral
+import 
+from pdf_parser  import parser_data
 
-def generate_podcast(podcast_text, speaker_1_description, speaker_2_description):
-    final_audio = None
-
-    for speaker, text in tqdm(ast.literal_eval(podcast_text), desc="Generating audiobook segments", unit="segment"):
-        if speaker == "Emma":
-            audio_segment = tts_server(text, speaker_1_description)
-        else:  # Speaker 2
-            audio_segment = tts_server(text, speaker_2_description)
-
-        # Add to final audio
-        if final_audio is None:
-            final_audio = audio_segment
-        else:
-            final_audio += audio_segment
-
-    final_audio.export("_audiobook_eng.mp3",
-                      format="mp3",
-                      bitrate="192k",
-                      parameters=["-q:a", "0"])
+def load_script_prompts(file_path):
+    with open(file_path, 'r') as file:
+        script_prompts = json.load(file)
+    return script_prompts
 
 def generate_speaker_audio(scenes_data):
     scenes = json.loads(scenes_data)['scenes']
@@ -100,26 +88,118 @@ def tts_server(text, speaker_description):
 
         # Convert the audio to a NumPy array
         return audio
+    
+def load_script_prompts(file_path):
+    with open(file_path, 'r') as file:
+        script_prompts = json.load(file)
+    return script_prompts
+
+def get_prompt_by_task_name_and_language(script_prompts, task_name, language):
+    for task in script_prompts["tasks"]:
+        if task["task_name"] == task_name:
+            for prompt in task["prompts"]:
+                if prompt["language"] == language:
+                    return prompt["prompt"]
+    return None
+
+def get_structured_scene(scene_text, script_prompts, language):
+    prompt_for_scene = get_prompt_by_task_name_and_language(script_prompts, "Script parser into Structured scene", language)
+    extracted_json = llm_parser(scene_text, prompt=prompt_for_scene)
+    # File path for the JSON file
+    file_path = 'structured_scene.json'
+
+    # Write the cleaned JSON data back to the file
+    with open(file_path, 'w') as json_file:
+        json.dump(extracted_json, json_file, indent=4)
+
+    # Read the JSON data from the file
+    with open(file_path, 'r') as json_file:
+        structure_json = json.load(json_file)
+
+    return structure_json
+
+def get_narrator_dialog(scene_text, script_prompts, language):
+    prompt_for_scene = get_prompt_by_task_name_and_language(script_prompts, "Prompt parser for narrator description", language)
+    extracted_json = llm_parser(scene_text, prompt=prompt_for_scene)
+    # File path for the JSON file
+    file_path = 'narrator_dialog.json'
+
+    # Write the cleaned JSON data back to the file
+    with open(file_path, 'w') as json_file:
+        json.dump(extracted_json, json_file, indent=4)
+
+    # Read the JSON data from the file
+    with open(file_path, 'r') as json_file:
+        structure_json = json.load(json_file)
+
+    return structure_json
+
+def get_speaker_dialog_voice_desc(scene_text, script_prompts, language):
+    prompt_for_scene = get_prompt_by_task_name_and_language(script_prompts, "Speaker Dialog Prompt for Parser", language)
+    extracted_json = llm_parser(scene_text, prompt=prompt_for_scene)
+    # File path for the JSON file
+    file_path = 'speaker_dialog_voice.json'
+
+    # Write the cleaned JSON data back to the file
+    with open(file_path, 'w') as json_file:
+        json.dump(extracted_json, json_file, indent=4)
+
+    # Read the JSON data from the file
+    with open(file_path, 'r') as json_file:
+        structure_json = json.load(json_file)
+
+    return structure_json
+
+def get_sound_effects_time_stamps(scene_text, script_prompts, language):
+    prompt_for_scene = get_prompt_by_task_name_and_language(script_prompts, "Prompt for background music and sound effect with timestamps", language)
+    extracted_json = llm_parser(scene_text, prompt=prompt_for_scene)
+    # File path for the JSON file
+    file_path = 'sound_effects_timestamp.json'
+
+    # Write the cleaned JSON data back to the file
+    with open(file_path, 'w') as json_file:
+        json.dump(extracted_json, json_file, indent=4)
+
+    # Read the JSON data from the file
+    with open(file_path, 'r') as json_file:
+        structure_json = json.load(json_file)
+
+    return structure_json
+def llm_parser(text, prompt):
+    try:
+        data = text
+
+        llm_prompt = f"Here is more data: {data}. Please answer the following question: {prompt}"            
+        model = "mistral-large-latest"
+        
+        system_prompt_german ="Please provide a concise answer in German. Output the response in a human-readable way in German (with paragraphs, etc.)."
+        messages = [
+            {
+                    "role": "system",
+                    "content": "Please provide a concise answer in English. Output the response in the requested format. Do not explain the output. Do not add anything new"            
+            },
+            {               
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": llm_prompt
+                    }
+                ]
+            }
+        ]
+
+        api_key = os.environ["MISTRAL_API_KEY"]
+        client = Mistral(api_key=api_key)
+        chat_response = client.chat.complete(model=model, messages=messages)
+        content = chat_response.choices[0].message.content
+
+        #print(content)
+        return content
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def main():
-    speaker_1_description = """
-            Emma's voice is expressive and dramatic in delivery, speaking at a moderately fast pace with a very close recording that almost has no background noise.
-        """
-    speaker_2_description = """
-            Leo's voice is deep and resonant, with a calm and authoritative tone. He speaks at a steady pace, ensuring clarity and precision in his delivery. The recording is clear with minimal background noise, providing a professional and engaging listening experience.
-        """
-
-    podcast_text = """
-    [
-        ('Emma', "So, Leo, what were you trying to show me here?"),
-        ('Leo', "Patience, Emma. It's a bit... how should I say... next-level cool."),
-        ('Emma', "Your surprises weren't always cool. Remember the super cave full of spiders."),
-        ('Leo', "Hey, that was an adventure! And there are no spiders this time. I promise."),
-    ]
-    """
-
-    podcast_text = podcast_text.strip()
-    generate_podcast(podcast_text, speaker_1_description, speaker_2_description)
 
     scenes_data_narrator = """
     {
@@ -210,8 +290,27 @@ def main():
         ]
     }
     """
+    '''
+    print("Audiobook Creation from Script")
+    local_filename = 'audiobook/resources/Skript-GoAudio-Eng.pdf'
+    scene_text = parser_data(local_filename)
 
+    # Load script prompts
+    script_prompts = load_script_prompts('script_prompts_eu.json')
 
+    structured_scene_json = get_structured_scene(scene_text=scene_text, script_prompts=script_prompts, language=language)
+    print(structured_scene_json)
+
+    narrator_dialog = get_narrator_dialog(structured_scene_json, script_prompts=script_prompts, language=language)
+    print(narrator_dialog)
+
+    speaker_voice_dialog = get_speaker_dialog_voice_desc(structured_scene_json, script_prompts=script_prompts, language=language)
+    print(speaker_voice_dialog)
+
+    sound_effects_timestamps = get_sound_effects_time_stamps(structured_scene_json, script_prompts=script_prompts, language=language)
+    print(sound_effects_timestamps)
+
+    '''
     generate_narrator_voice(scenes_data_narrator)
 
     generate_speaker_audio(scenes_data)
@@ -219,4 +318,7 @@ def main():
     combine_audio_segments(scenes_data)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Audiobook Creation from Script")
+    parser.add_argument('--language', type=str, default='en', help='Language code for the prompts (e.g., en, fr, de, es, it, nl, pt, sv)')
+    args = parser.parse_args()
+    main(args.language)
