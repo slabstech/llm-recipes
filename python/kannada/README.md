9c85de57ee4a2f442c3edcbc9b56433c55bb8cfa
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-02-23 20:24:13 +0100

    update references

--- python/kannada/README.md
+++ python/kannada/README.md
@@ -1,7 +1,27 @@
 Kannada 
 
+- End to End Flow
+    - ASR - Automatic Speech Recognition
+    - Translation 
+    - TTS - Text to Speech
+
 Tools for Kannada
 
 - ASR 
     - ARTPARK-IISc/whisper-medium-vaani-kannada
     - huggingface-cli download ARTPARK-IISc/whisper-medium-vaani-kannada
+
+- Source
+    - https://github.com/slabstech/asr-indic-server
+    - https://github.com/slabstech/indic-translate-server
+    - https://github.com/slabstech/parler-tts-server
+
+- Docker images
+    - TTS - slabstech/parler-tts-server:latest
+    - Translate - slabstech/translate-indic-server 
+    - ASR - slabstech/asr-indic-server
+
+- Reference
+    - https://huggingface.co/ARTPARK-IISc/whisper-medium-vaani-kannada
+
+-
--- python/kannada/src/asr.py
+++ python/kannada/src/asr.py
@@ -1,37 +1,36 @@
-
 import torch
-from transformers import WhisperForConditionalGeneration, WhisperProcessor, WhisperTokenizer,WhisperFeatureExtractor
+from transformers import WhisperForConditionalGeneration, WhisperProcessor, WhisperTokenizer, WhisperFeatureExtractor
 import soundfile as sf
+import torchaudio
 
 
-model="ARTPARK-IISc/whisper-small-vaani-medium"
+model = "ARTPARK-IISc/whisper-medium-vaani-kannada"
+#model = "ARTPARK-IISc/whisper-small-vaani-kannada"
 
 # Load tokenizer and feature extractor individually
 feature_extractor = WhisperFeatureExtractor.from_pretrained(model)
-tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-medium", language="Kannada", task="transcribe")
-
-
+#tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-medium", language="Kannada", task="transcribe")
+tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-medium", language="Kannada", task="translate")
 # Create the processor manually
 processor = WhisperProcessor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
 # Load and preprocess the audio file
-audio_file_path = "Sample_Audio.wav"  # replace with your audio file path
-
+audio_file_path = "../data/kannada_2.wav"  # replace with your audio file path
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load the processor and model
 model = WhisperForConditionalGeneration.from_pretrained(model).to(device)
 
-
 # load audio
 audio_data, sample_rate = sf.read(audio_file_path)
+
 # Ensure the audio is 16kHz (Whisper expects 16kHz audio)
 if sample_rate != 16000:
-    import torchaudio
+    # Convert audio data to float32
+    audio_data = audio_data.astype('float32')
     resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)
     audio_data = resampler(torch.tensor(audio_data).unsqueeze(0)).squeeze().numpy()
-
 
 # Use the processor to prepare the input features
 input_features = processor(audio_data, sampling_rate=16000, return_tensors="pt").input_features.to(device)
