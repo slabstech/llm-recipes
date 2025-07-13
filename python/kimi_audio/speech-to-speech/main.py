f1b7eace8f28b0014416b5b8854f9a317e917856
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-07-13 11:08:59 +0200

    add-states

--- python/kimi_audio/speech-to-speech/main.py
+++ python/kimi_audio/speech-to-speech/main.py
@@ -1,67 +1,160 @@
-def client_send_handshake():
+def client_send_handshake(audio_connection):
     print("client -sending handhsake")
+    audio_connection = {"client_connection":"true"}
+    return audio_connection
 
-def server_receive_handshake():
+def server_receive_handshake(audio_connection):
     print("server-receive_handhsake")
+    audio_connection_new = {"server_connection":"true"}
 
-def client_send_audio_chunk():
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+
+def client_send_audio_chunk(audio_connection):
     print("cleint send audio chunk")
+    audio_connection_new = {"client_send_audio_chunk=1":"true"}
 
-def server_receive_audio_chunk():
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+
+
+def server_receive_audio_chunk(audio_connection):
     print("server receive audio chunk")
+    audio_connection_new = {"server_receive_audio_chunk=1":"true"}
 
-def server_send_vad_commit_message():
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+
+def server_send_vad_commit_message(audio_connection):
     print("server_send_vad_commit_message")
+    audio_connection_new = {"server_send_vad_commit_message":"true"}
 
-def client_receive_vad_message():
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+
+def client_receive_vad_message(audio_connection):
     print("client_receive_vad_message")
 
-def server_send_audio_chunk():
-    print("server_send_audio_chunk")
+    audio_connection_new = {"client_receive_vad_message":"true"}
 
-def client_receive_audio_chunk():
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+def server_send_audio_chunk(audio_connection):
+    print("server_send_audio_chunk")
+    audio_connection_new = {"server_send_audio_chunk=1":"true"}
+
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+def client_receive_audio_chunk(audio_connection):
     print("client_receive_audio_chunk")
 
+    audio_connection_new = {"client_receive_audio_chunk=1":"true"}
 
-def server_send_finish():
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+def server_send_finish(audio_connection):
     print("server_send_finish")
+    audio_connection_new = {"server_send_finish":"true"}
 
-def client_receive_finish():
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+def client_receive_finish(audio_connection):
     print("client_receive_finish")
-    
+    audio_connection_new = {"client_receive_finish":"true"}
+
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+
+def client_end_connection(audio_connection):
+    print("client_end_connection")
+    audio_connection_new = {"client_end_connection":"true"}
+
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+def server_end_connection(audio_connection):
+    print("server_end_connection")
+    audio_connection_new = {"server_end_connection":"true"}
+
+    audio_connection |= audio_connection_new
+    return audio_connection
+
+
+
 def main():
     print("hwllo_wolrd")
 
-    client_send_handshake()
+    audio_connection={}
 
-    server_receive_handshake()
+    audio_connection=client_send_handshake(audio_connection)
 
+    print(audio_connection)
+    server_receive_handshake(audio_connection)
 
-    client_send_audio_chunk()
+    print(audio_connection)
 
-    server_receive_audio_chunk()
-
-
-    client_send_audio_chunk()
-
-    server_receive_audio_chunk()
-
-    server_send_vad_commit_message()
-
-    client_receive_vad_message()
+    client_send_audio_chunk(audio_connection)
 
 
-    server_send_audio_chunk()
-    client_receive_audio_chunk()
+    print(audio_connection)
+
+    server_receive_audio_chunk(audio_connection)
+
+    print(audio_connection)
+
+    client_send_audio_chunk(audio_connection)
+
+    print(audio_connection)
+
+    server_receive_audio_chunk(audio_connection)
+
+    print(audio_connection)
+
+    server_send_vad_commit_message(audio_connection)
+    print(audio_connection)
 
 
-    server_send_audio_chunk()
-    client_receive_audio_chunk()
-    server_send_audio_chunk()
-    client_receive_audio_chunk()
 
-    server_send_finish()
-    client_receive_finish()
+    client_receive_vad_message(audio_connection)
+
+    print(audio_connection)
+
+    server_send_audio_chunk(audio_connection)
+
+    print(audio_connection)
+
+    client_receive_audio_chunk(audio_connection)
+
+    print(audio_connection)
+
+    server_send_audio_chunk(audio_connection)
+
+    print(audio_connection)
+    client_receive_audio_chunk(audio_connection)
+    print(audio_connection)
+    server_send_audio_chunk(audio_connection)
+    print(audio_connection)
+    client_receive_audio_chunk(audio_connection)
+    print(audio_connection)
+
+    server_send_finish(audio_connection)
+    print(audio_connection)
+    client_receive_finish(audio_connection)
+    print(audio_connection)
+
+    client_end_connection(audio_connection)
+    print(audio_connection)
+
+    server_end_connection(audio_connection)
     
 if __name__ == "__main__":
     main()
